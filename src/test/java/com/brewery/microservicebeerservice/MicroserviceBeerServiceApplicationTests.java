bfae6befd93f295a2c422d9f5780a9cd1f45a1ce
Benedict Su <benedict.suratanakavikul@zenjob.com>
2023-05-30 16:52:25 +0200

    init spring project

---
+++ src/test/java/com/brewery/microservicebeerservice/MicroserviceBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.microservicebeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
